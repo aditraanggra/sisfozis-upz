274e7e9a95ef40cb5e2059a79c842e9e5b15a670
aditraanggra <aditra.anggra@gmail.com>
2025-03-10 02:01:57 +0700

    register middleware for forces https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->append(\App\Http\Middleware\ForceHttpsLivewire::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
