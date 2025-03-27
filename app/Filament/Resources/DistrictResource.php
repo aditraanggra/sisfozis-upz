82d70336ac7a3d2edbdffc3831f6d0c899f025e3
aditraanggra <aditra.anggra@gmail.com>
2025-03-27 09:55:06 +0700

    add ditrict exporter

--- app/Filament/Resources/DistrictResource.php
+++ app/Filament/Resources/DistrictResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources;
 
+use App\Filament\Exports\DistrictExporter;
 use App\Filament\Resources\DistrictResource\Pages;
 use App\Filament\Resources\DistrictResource\RelationManagers;
 use App\Models\District;
@@ -12,6 +13,8 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ExportAction; // Ensure this is the correct namespace for ExportAction
+use Filament\Tables\Actions\ExportAction as ActionsExportAction;
 
 class DistrictResource extends Resource
 {
@@ -149,6 +152,10 @@ class DistrictResource extends Resource
             ])
             ->bulkActions([
                 //
+            ])
+            ->headerActions([
+                ActionsExportAction::make()
+                    ->exporter(DistrictExporter::class)
             ])
             //->defaultSort('total', 'desc')
             ->recordUrl(null);
